d8aaa5eff28fd99e8328d4b3f4af8948ecd22fc5
CrazyGremlin <michael.wittek@gamesdynamics.com>
2015-05-06 20:05:00 +0200

    Initial Commit

---
+++ ios-Facebook-InviteFriends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-Facebook-InviteFriends
+//
+//  Created by Michael Wittek on 06.05.15.
+//  Copyright (c) 2015 Michael Wittek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-Facebook-InviteFriendsTests/ios_Facebook_InviteFriendsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_Facebook_InviteFriendsTests.swift
+//  ios-Facebook-InviteFriendsTests
+//
+//  Created by Michael Wittek on 06.05.15.
+//  Copyright (c) 2015 Michael Wittek. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ios_Facebook_InviteFriendsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
